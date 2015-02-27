d056ab67be635f0e53dd2745c02548a047f7685c
terrence <parliamentaryterrence@gmail.com>
2015-02-26 19:59:54 -0500

    Updated tests

--- test/interpolateSpec.js
+++ test/interpolateSpec.js
@@ -2,29 +2,37 @@
  * Created by terrence on 2/26/15.
  */
 var expect = require("chai").expect;
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var chai = require("chai");
+chai.use(sinonChai);
 var proxyquire = require("proxyquire");
+
 describe("interpolator service", function(){
 
     beforeEach(function(){
-        this.interpolator = proxyquire("../lib/interpolate.js", {
-            fs: {
-                readFileSync: function(){
-                    return JSON.stringify([
-                        {
-                            name: "Jones",
-                            age: 21
-                        },
-                        {
-                            name: "Smith",
-                            age: 24
-                        }
-                    ]);
-                }
+
+        this.fsStub = sinon.stub();
+        this.fsStub.returns(JSON.stringify([
+            {
+                "name": "Smith",
+                "age": 21
+            },
+            {
+                "name": "Jones",
+                "age": 25
+                
             }
-        })
+        ]));
+        //this.interpolator = require("../lib/interpolate.js");
+        this.interpolator = proxyquire("../lib/interpolate.js", {fs: {
+            readFileSync: this.fsStub
+            
+        }});
+
     });
 
-    describe("parse method", function(){
+    describe("parse method - default options", function(){
 
         it("should, given a string of JSON with no interpolation, return that string of JSON unmodified", function(){
             var str = JSON.stringify({
@@ -46,6 +54,29 @@ describe("interpolator service", function(){
 
             var result = this.interpolator.parse(str);
             expect(result.indexOf("Jones")).to.not.equal(-1);
+        });
+        
+        it("should cache files instead of reading them twice", function(){
+            var spy = sinon.spy(this.fsStub.readFileSync);
+
+            var str = JSON.stringify([
+                    {
+
+                        "name": "Terrence",
+                        "friends": "{{ import 'friends.json' }}"
+                    },
+                    {
+                        "name": "Heather",
+                        "friends": "{{ import 'friends.json' }}"
+
+                    }
+                ]
+            );
+            
+            var result = this.interpolator.parse(str);
+            expect(this.fsStub).to.have.been.calledOnce;
+            
+            
         });
     });
 
